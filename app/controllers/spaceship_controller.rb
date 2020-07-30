41107e9c1a9b084c7dc1039595ef50fdbfe42761
Rida Serroukh <rida.serroukh@gmail.com>
2020-07-30 20:06:11 +0100

    Spaceship controller

---
+++ app/controllers/spaceship_controller.rb
@@ -0,0 +1,32 @@
+class SpapceshipsController < ApplicationController
+	before_action :authenticate_user!
+
+    def index
+      @spaceships = Spaceship.all
+    end
+
+    def new
+      @spaceship = Spaceship.new
+    end
+
+    def create
+			@spaceship = Spaceship.new
+			if @spaceship.save
+				redirect_to spaceship_path(@spaceship)
+			else
+				render 'new'
+		end
+	end
+
+		def show
+		@spaceship = Spaceship.show
+		end
+
+		def update
+			if @spaceship.update(spaceship.params)
+				redirect_to spaceship_path
+			else
+				render 'edit'
+			end
+		end
+
