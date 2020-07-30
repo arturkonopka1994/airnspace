adfd7d8339b14409b4afffe6cf70bdbada01244e
Artur Konopka <artur.konopka.1994@gmail.com>
2020-07-30 20:59:38 +0100

    Controller issue fixed

--- app/controllers/spaceship_controller.rb
+++ app/controllers/spaceship_controller.rb
@@ -2,49 +2,49 @@ class SpapceshipsController < ApplicationController
 	before_action :set_spaceship, only: [:show, :update, :destroy, :edit] 
 	skip_before_action :authenticate_user!, only: :home, :index, :show
 
-    def index
-      @spaceships = Spaceship.all
-    end
+	def index
+		@spaceships = Spaceship.all
+	end
 
-    def new
-      @spaceship = Spaceship.new
-    end
+	def new
+		@spaceship = Spaceship.new
+	end
 
-    def create
-			@spaceship = Spaceship.new
-			if @spaceship.save
-				redirect_to spaceship_path(@spaceship)
-			else
-				render 'new'
+	def create
+		@spaceship = Spaceship.new
+		if @spaceship.save
+			redirect_to spaceship_path(@spaceship)
+		else
+			render 'new'
+	end
+
+	def show			
+	end
+
+	def update
+		if @spaceship.update(spaceship.params)
+			redirect_to spaceship_path
+		else
+			render 'edit'
 		end
 	end
 
-		def show			
-		end
+	def edit
+	end
 
-		def update
-			if @spaceship.update(spaceship.params)
-				redirect_to spaceship_path
-			else
-				render 'edit'
-			end
-		end
+	def destroy
+		@spaceship.destroy
+		redirect_to spaceship_path
+	end
 
-		def edit
-		end
+	private
 
-		def destroy
-			@spaceship.destroy
-			redirect_to spaceship_path
-		end
+	def set_spaceship
+		@spaceship = Spaceship.find(params[:id])
+	end
 
-		private
-
-		def set_spaceship
-			@spaceship = Spaceship.find(params[:id])
-		end
-
-		def spaceship_params
-			params.require(:spaceship).permit(:model)
-		end
+	def spaceship_params
+		params.require(:spaceship).permit(:model)
+	end
+end
 
