44b0d44d4eb6ddcddbf915fbdec9099658543f0e
matt evans <mattevo10@gmail.com>
2020-08-01 13:45:54 +0100

    fixing relationships for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :bookings, :reviews, :spaceships, dependent: :destroy
+  has_many :bookings, dependent: :destroy
+  has_many :reviews, dependent: :destroy
+  has_many :spaceships, dependent: :destroy
+  has_many :spaceships, through: :booking
 end
