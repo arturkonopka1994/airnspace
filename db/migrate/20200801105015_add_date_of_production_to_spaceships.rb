9062b0d3c15b7de314d2fc902a299d9ee0b8854e
Artur Konopka <artur.konopka.1994@gmail.com>
2020-08-01 15:39:08 +0100

    Seed working now

--- db/migrate/20200801105015_add_date_of_production_to_spaceships.rb
+++ db/migrate/20200801105015_add_date_of_production_to_spaceships.rb
@@ -1,5 +1,5 @@
 class AddDateOfProductionToSpaceships < ActiveRecord::Migration[6.0]
   def change
-    add_column :spaceships, :date_of_production, :date
+    add_column :spaceships, :date_of_production, :integer
   end
 end
