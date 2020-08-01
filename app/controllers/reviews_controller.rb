3468bb5e283c864e143fa516cff9f68bceee5994
Rida Serroukh <rida.serroukh@gmail.com>
2020-08-01 11:40:40 +0100

    reviews-controller done

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,19 @@
+class ReviewsController < ApplicationController
+    def create
+      @spaceship = Spaceship.find(params[:reviews_id])
+      @review = Review.new(review_params)
+      @review.spaceship = @spaceship
+      if @review.save
+        redirect_to spaceship_path(@spaceship)
+      else
+        @spaceship = Spaceship.new
+        render "reviews/show"
+      end
+    end
+  
+    private
+  
+    def review_params
+      params.require(:review).permit(:rating, :description)
+    end
+  end
