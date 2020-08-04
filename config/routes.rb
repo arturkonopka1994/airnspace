69568ddd2910a71abd4243395e322c5fc243e6c5
matt evans <mattevo10@gmail.com>
2020-08-04 19:26:05 +0100

    added bookings view

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   end
 
   resources :bookings, only: [:destroy, :show]
+  get "/dashboard", to: "pages#dashboard"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
