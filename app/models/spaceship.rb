17bf3337f0c000378c586090906df4c51041f5c0
matt evans <mattevo10@gmail.com>
2020-08-08 12:07:28 +0100

    drop down box

--- app/models/spaceship.rb
+++ app/models/spaceship.rb
@@ -4,4 +4,9 @@ class Spaceship < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_one_attached :photo
   validates :model, :capacity, presence: true
+
+  def country_name
+    country = ISO3166::Country[location]
+    country.translations[I18n.locale.to_s] || country.name
+  end
 end
